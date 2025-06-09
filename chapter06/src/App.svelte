b883732d20069176b64a9d7b37f7948c897cc402
김규민 <abc2232002@gmail.com>
2025-06-09 22:56:15 +0900

    이벤트 처리리

---
+++ chapter06/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import GoGoogle from "./GoGoogle.svelte";
+
+  const handleClick1 = () => alert('한번만 제공합니다.')
+  const handleClick2 = () => alert('클릭이 계속 실행됩니다.')
+</script>
+
+<button on:click|once={handleClick1}>1번 버튼</button>
+<button on:click={handleClick2}>2번 버튼</button>
+
+<GoGoogle />
---
+++ chapter06/src/GoGoogle.svelte
@@ -0,0 +1,10 @@
+<script>
+  const handleClick =() => {
+    let move = confirm('구글로 이동하시겠습니까?')
+    move && location.assign('https://www.google.com')
+  }
+</script>
+
+<h1>
+  <a href="https://www.google.com" on:click|preventDefault={handleClick}>구글 바로가기</a>
+</h1>
